8958e80912e1ec75a94fa34bc333f2723914e894
misguelo <110464716+misguelo@users.noreply.github.com>
2024-12-04 16:28:22 -0300

    Add files via upload

---
+++ Miguel/perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+    'beneficios de praticar esportes',
+    ' vôlei',
+    'Estimula a sinapse dos neurônios, pois precisa estudar a jogada, o adversário, o time. Melhora a cognição, porque necessita ter a percepção rápida da jogada. Fortalece o corpo, estimula os membros superiores e inferiores, aumentando a flexibilidade e a coordenação motora. Melhora as capacidades cardiorrespiratórias.S'
+)
+
+ 
+
+criaCartao(
+    'Futebol',
+
+    'Um benefício imediato da prática do futebol é a melhoria do bem-estar. A médio e longo prazos, observa-se também a melhoria da força, da velocidade, da flexibilidade, da coordenação, da agilidade, da resistência e da potência. Com isso, ocorrem benefícios psicológicos, estéticos e de saúde.'
+)
+
+criaCartao(
+    'Basquete',
+
+    'Jogar basquete ajuda a melhorar a coordenação motora, flexibilidade e resistência. Também estimula a velocidade, agilidade e força.'
+)
